4e7ce3771dfa3f9eabe9c18fe717c309aa153f27
VincentPat <hys510573094@gmail.com>
2017-09-01 23:06:45 +0800

    fix demo link error

--- webpack.config.js
+++ webpack.config.js
@@ -15,16 +15,6 @@ if (process.env.NODE_ENV === 'development') {
     };
 }
 
-// example
-if (process.env.NODE_ENV === 'example') {
-    var entry = './src/dev.js';
-    var output = {
-        path: path.resolve(__dirname, './dist'),
-        publicPath: '/examples/',
-        filename: 'dev.js'
-    };
-}
-
 // production
 if (process.env.NODE_ENV === 'production') {
     entry = './index.js';
